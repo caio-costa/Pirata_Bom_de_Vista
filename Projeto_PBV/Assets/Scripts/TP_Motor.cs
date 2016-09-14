af19c23762b8dbf238567493dbf2acbc7bc80b5f
Caio Costa <caios.costa@hotmail.com>
2016-09-13 23:01:44 -0300

    Controlador de Player e Câmera v1

---
+++ Projeto_PBV/Assets/Scripts/TP_Motor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TP_Motor : MonoBehaviour {
+
+    public static TP_Motor Instance;
+    public float MoveSpeed = 10f;
+    public Vector3 MoveVector { get; set; }
+
+	void Awake() {
+        Instance = this;
+	}
+	
+	public void UpdateMotor() {
+        SnapAlignCharacterWithCamera();
+        ProcessMotion();
+    }
+
+    void ProcessMotion(){
+        //Transforma MoveVector em espaço do mundo
+        MoveVector = transform.TransformDirection(MoveVector);
+
+        //Normalizar MoveVector caso tamanho > 1
+        if (MoveVector.magnitude > 1)
+            MoveVector = Vector3.Normalize(MoveVector);
+
+        //Multiplicar MoveVector por MoveSpeed
+        MoveVector *= MoveSpeed;
+
+        //Multiplicar MoveVector por DeltaTime
+        MoveVector *= Time.deltaTime;
+
+        //Mover o personagem pelo espaço do mundo
+        TP_Controller.CharacterController.Move(MoveVector);
+    }
+
+    void SnapAlignCharacterWithCamera(){
+        if(MoveVector.x != 0 || MoveVector.z != 0){
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x, 
+                    Camera.main.transform.eulerAngles.y, 
+                    transform.eulerAngles.z);
+        }
+    }
+}
